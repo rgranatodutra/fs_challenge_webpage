746e6d92ee6e451a6460b745d696bac4d1de0183
Renan G. Dutra <r.granatodutra@gmail.com>
2023-08-07 07:48:15 -0300

    feat: add toast container and css

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
+import { ToastContainer } from "react-toastify";
 import Body from "./components/body";
 import Header from "./components/header";
 import { ClientsProvider } from "./contexts/clientsContext/clientsContext";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function App() {
 	return (
@@ -9,6 +11,7 @@ export default function App() {
 			<ClientsProvider>
 				<Body />
 			</ClientsProvider>
+			<ToastContainer />
 		</>
 	);
 }
